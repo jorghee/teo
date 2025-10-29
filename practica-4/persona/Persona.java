ef6462cd52f612f942bdd01f709b1c096ffe6dc0
Michael Benjamin Suclle Suca <michaelsuclle@gmail.com>
2025-10-28 23:19:21 -0500

    indentando con 2 espacios

--- practica-4/persona/Persona.java
+++ practica-4/persona/Persona.java
@@ -1,49 +1,49 @@
 package persona;
 
 public abstract class Persona {
-	protected String id;
-	protected String nombre;
-	protected String apellido;
-	protected String email;
-	protected String telefono;
+  protected String id;
+  protected String nombre;
+  protected String apellido;
+  protected String email;
+  protected String telefono;
 
-	public Persona(String id, String nombre, String apellido, String email, String telefono) {
-		this.id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.email = email;
-		this.telefono = telefono;
-	}
+  public Persona(String id, String nombre, String apellido, String email, String telefono) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.telefono = telefono;
+  }
 
-	public String getId() {
-		return id;
-	}
+  public String getId() {
+    return id;
+  }
 
-	public String getNombre() {
-		return nombre;
-	}
+  public String getNombre() {
+    return nombre;
+  }
 
-	public String getApellido() {
-		return apellido;
-	}
+  public String getApellido() {
+    return apellido;
+  }
 
-	public String getEmail() {
-		return email;
-	}
+  public String getEmail() {
+    return email;
+  }
 
-	public String getTelefono() {
-		return telefono;
-	}
+  public String getTelefono() {
+    return telefono;
+  }
 
-	// comportamiento por defecto al registrar una persona; las subclases pueden sobreescribirlo
-	public void registrar() {
-		System.out.println("Persona " + getNombreCompleto() + " registrada.");
-	}
+  // comportamiento por defecto al registrar una persona; las subclases pueden sobreescribirlo
+  public void registrar() {
+    System.out.println("Persona " + getNombreCompleto() + " registrada.");
+  }
 
-	// cada subclase debe indicar su tipo (Estudiante, Docente, Participante, Ponente, etc.)
-	public abstract String obtenerTipo();
+  // cada subclase debe indicar su tipo (Estudiante, Docente, Participante, Ponente, etc.)
+  public abstract String obtenerTipo();
 
-	public String getNombreCompleto() {
-		return nombre + " " + apellido;
-	}
+  public String getNombreCompleto() {
+    return nombre + " " + apellido;
+  }
 }
