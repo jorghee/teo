3ad265f471336fc29d5c0e11cfac2b8398d92131
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-12 15:36:53 -0500

    Desarrollo de funciones base

--- prac07/3. Conexión simulada a base de datos/ConexionBD.cpp
+++ prac07/3. Conexión simulada a base de datos/ConexionBD.cpp
@@ -3,23 +3,47 @@
 
 class ConexionBD {
 private:
-    static ConexionBD* sgtn;  
-    bool conectado;                 
+    static ConexionBD* sgtn;  // singleton
+    bool conectada;                 
 
-    ConexionBD() : conectado(false) {}
+    ConexionBD() : conectada(false) {}
 
 public:
-
     static ConexionBD* getInstancia() {
-        
+        if (sgtn == nullptr) {
+            sgtn = new ConexionBD();
+            std::cout << "Se creo la conexion\n";
+        }
+        return sgtn;
     }
 
     void conectar() {
+        if (!conectada) {
+            conectada = true;
+            std::cout << "Conectada\n";
+        } else {
+            std::cout << "Ya hay una conexion activa\n";
+        }
     }
 
     void desconectar() {
+        if (conectada) {
+            conectada = false;
+            std::cout << "Conexión cerrada\n";
+        } else {
+            std::cout << "No hay ninguna conexion activa \n";
+        }
     }
 
-    void estado(){}
-    
+    void estado() const {
+        if (conectada)
+            std::cout << "- Conectado\n";
+        else
+            std::cout << "- Desconectado\n";
+    }
 };
+
+ConexionBD* ConexionBD::sgtn = nullptr;
+
+
+
